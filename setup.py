00534f96892f38f40f35439a2ebff74a7a640621
Yiming Liu <YimingDesigner@gmail.com>
2023-12-25 11:37:17 +0800

    fix: change version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="socket_kit",
-    version="0.1.0",
+    version="0.1.1",
     description="A utility with concise and more fluent code in socket programming",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
